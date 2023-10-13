cc115886cc9e7851e17d04d2aa85989d494ab286
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-10-14 00:19:46 +0200

    fix errors and new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain bg-transparent" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Marcos &nbsp;
             <span className="sm:block hidden">| Corrales Castro</span>
