b1c5f7d1ec5c51cccfb9dba5ad2734dce723f799
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-10-13 23:42:17 +0200

    implement emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,43 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs.send(
+      env,
+      'template_huebl0p',
+      {
+        from_name: form.name,
+        to_name: 'Marcos',
+        from_email: form.email,
+        to_email: 'marcosdecoa@gmail.com',
+        message: form.message,
+      },
+      'MT8egtC5h3RQno5ZL'
+    )
+    .then(() => {
+      setLoading(false)
+      alert('Thank you for your message! I will get back to you as soon as possible.')
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -67,7 +101,7 @@ const Contact = () => {
             <textarea
               rows="7"
               name="message"
-              value={form.name}
+              value={form.message}
               onChange={handleChange}
               placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border font-medium"
