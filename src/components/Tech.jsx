2a6b2fdf8a7058a4f3639f7e254bd15e6914a760
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-10-16 19:29:26 +0200

    fix canvas mobile error

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,28 @@
 import { BallCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
+import { useEffect, useState } from 'react';
 
 const Tech = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isSmallScreen = windowWidth < 576;
+  const displayedTechnologies = isSmallScreen ? technologies.slice(0, 5) : technologies;
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {displayedTechnologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
