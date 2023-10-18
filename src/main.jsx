4dc55f0ad9e25ca7e42a24dcd624530d4af6a782
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-10-18 19:41:33 +0200

    i18n install

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { I18NProvider } from 'react-i18next'
+import i18next from 'i18next'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <I18NProvider i18n={i18next}>
+      <App />
+    </I18NProvider>
   </React.StrictMode>,
 )
